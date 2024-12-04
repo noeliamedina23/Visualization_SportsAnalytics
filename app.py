b9e39edd1e2c6daa1d12206930fbfc472f803e1e
noeliamedina23 <133890247+noeliamedina23@users.noreply.github.com>
2024-12-04 19:24:30 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+# Título del Dashboard
+st.title("Mi Dashboard en Streamlit")
+
+# Aquí puedes agregar más componentes como gráficos, tablas, etc.
+st.write("Este es un ejemplo básico de Streamlit.")
